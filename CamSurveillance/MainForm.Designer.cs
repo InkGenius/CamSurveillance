138e3253273680420c6f7cafa07b7f89ac4552e2
InkGenius <daisong1860@hotmail.com>
2017-10-05 15:34:38 +0800

    FEAT:完成视频监控基本功能

---
+++ CamSurveillance/MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CamSurveillance
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.本地摄像头ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开JPEGURLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jPEGURLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mJPEGURLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panelUrl = new System.Windows.Forms.Panel();
+            this.btnLink = new System.Windows.Forms.Button();
+            this.comboBoxUrls = new System.Windows.Forms.ComboBox();
+            this.labelTip = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.panelUrl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(0, 71);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(657, 343);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.Text = "videoSourcePlayer";
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.本地摄像头ToolStripMenuItem,
+            this.打开JPEGURLToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(657, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 本地摄像头ToolStripMenuItem
+            // 
+            this.本地摄像头ToolStripMenuItem.Name = "本地摄像头ToolStripMenuItem";
+            this.本地摄像头ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.本地摄像头ToolStripMenuItem.Text = "本地摄像头";
+            this.本地摄像头ToolStripMenuItem.Click += new System.EventHandler(this.本地摄像头ToolStripMenuItem_Click);
+            // 
+            // 打开JPEGURLToolStripMenuItem
+            // 
+            this.打开JPEGURLToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jPEGURLToolStripMenuItem,
+            this.mJPEGURLToolStripMenuItem});
+            this.打开JPEGURLToolStripMenuItem.Name = "打开JPEGURLToolStripMenuItem";
+            this.打开JPEGURLToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.打开JPEGURLToolStripMenuItem.Text = "远程摄像头";
+            // 
+            // jPEGURLToolStripMenuItem
+            // 
+            this.jPEGURLToolStripMenuItem.Name = "jPEGURLToolStripMenuItem";
+            this.jPEGURLToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.jPEGURLToolStripMenuItem.Text = "JPEG URL";
+            this.jPEGURLToolStripMenuItem.Click += new System.EventHandler(this.jPEGURLToolStripMenuItem_Click);
+            // 
+            // mJPEGURLToolStripMenuItem
+            // 
+            this.mJPEGURLToolStripMenuItem.Name = "mJPEGURLToolStripMenuItem";
+            this.mJPEGURLToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.mJPEGURLToolStripMenuItem.Text = "MJPEG URL";
+            this.mJPEGURLToolStripMenuItem.Click += new System.EventHandler(this.mJPEGURLToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 417);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(657, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // panelUrl
+            // 
+            this.panelUrl.Controls.Add(this.btnLink);
+            this.panelUrl.Controls.Add(this.comboBoxUrls);
+            this.panelUrl.Controls.Add(this.labelTip);
+            this.panelUrl.Location = new System.Drawing.Point(3, 28);
+            this.panelUrl.Name = "panelUrl";
+            this.panelUrl.Size = new System.Drawing.Size(654, 37);
+            this.panelUrl.TabIndex = 3;
+            // 
+            // btnLink
+            // 
+            this.btnLink.Location = new System.Drawing.Point(538, 8);
+            this.btnLink.Name = "btnLink";
+            this.btnLink.Size = new System.Drawing.Size(75, 23);
+            this.btnLink.TabIndex = 2;
+            this.btnLink.Text = "连接";
+            this.btnLink.UseVisualStyleBackColor = true;
+            this.btnLink.Click += new System.EventHandler(this.btnLink_Click);
+            // 
+            // comboBoxUrls
+            // 
+            this.comboBoxUrls.FormattingEnabled = true;
+            this.comboBoxUrls.Location = new System.Drawing.Point(111, 10);
+            this.comboBoxUrls.Name = "comboBoxUrls";
+            this.comboBoxUrls.Size = new System.Drawing.Size(399, 20);
+            this.comboBoxUrls.TabIndex = 1;
+            // 
+            // labelTip
+            // 
+            this.labelTip.AutoSize = true;
+            this.labelTip.Location = new System.Drawing.Point(9, 13);
+            this.labelTip.Name = "labelTip";
+            this.labelTip.Size = new System.Drawing.Size(53, 12);
+            this.labelTip.TabIndex = 0;
+            this.labelTip.Text = "labelTip";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 439);
+            this.Controls.Add(this.panelUrl);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "摄像头通信";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.panelUrl.ResumeLayout(false);
+            this.panelUrl.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 本地摄像头ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开JPEGURLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jPEGURLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mJPEGURLToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel panelUrl;
+        private System.Windows.Forms.Label labelTip;
+        private System.Windows.Forms.Button btnLink;
+        private System.Windows.Forms.ComboBox comboBoxUrls;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
+
---
+++ CamSurveillance/MainForm.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Diagnostics;
+
+namespace CamSurveillance
+{
+    public partial class MainForm : Form
+    {
+        private Stopwatch stopWatch = null;
+        private enum LINKTYPE { JPEG, MJPEG };
+        private LINKTYPE currentChoice = LINKTYPE.JPEG;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.panelUrl.Visible = false;
+        }
+
+        private void 本地摄像头ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VideoCaptureDeviceForm form = new VideoCaptureDeviceForm();
+
+            if (form.ShowDialog(this) == DialogResult.OK)
+            {
+                // create video source
+                VideoCaptureDevice videoSource = form.VideoDevice;
+
+                // open it
+                OpenVideoSource(videoSource);
+            }
+        }
+
+        private void jPEGURLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.panelUrl.Visible = true;
+            this.labelTip.Text = "请输入JEPG URL";
+            this.comboBoxUrls.Text = "";
+            currentChoice = LINKTYPE.JPEG;
+            string[] URLs = new string[]
+            {
+                "http://www.gtc.iac.es/multimedia/netcam/camaraExt.jpg",
+                "http://217.168.94.131/axis-cgi/jpg/image.cgi?resolution=320x240"
+            };
+            comboBoxUrls.Items.Clear();
+            comboBoxUrls.Items.AddRange(URLs);
+        }
+
+        private void mJPEGURLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.panelUrl.Visible = true;
+            this.labelTip.Text = "请输入MJEPG URL";
+            this.comboBoxUrls.Text = "";
+            currentChoice = LINKTYPE.MJPEG;
+            string[] URLs = new string[]
+            {
+                "http://towercam.uu.edu/axis-cgi/mjpg/video.cgi"
+            };
+            comboBoxUrls.Items.Clear();
+            comboBoxUrls.Items.AddRange(URLs);
+        }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // stop current video source
+            CloseCurrentVideoSource();
+
+            // start new video source
+            videoSourcePlayer.VideoSource = source;
+            videoSourcePlayer.Start();
+
+            // reset stop watch
+            stopWatch = null;
+
+            // start timer
+            timer.Start();
+
+            this.Cursor = Cursors.Default;
+        }
+
+        // Close video source if it is running
+        private void CloseCurrentVideoSource()
+        {
+            if (videoSourcePlayer.VideoSource != null)
+            {
+                videoSourcePlayer.SignalToStop();
+
+                // wait ~ 3 seconds
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!videoSourcePlayer.IsRunning)
+                        break;
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                if (videoSourcePlayer.IsRunning)
+                {
+                    videoSourcePlayer.Stop();
+                }
+
+                videoSourcePlayer.VideoSource = null;
+            }
+        }
+
+        private void btnLink_Click(object sender, EventArgs e)
+        {
+            string url = comboBoxUrls.Text;
+            if (url.Trim().Length == 0)
+            {
+                MessageBox.Show("请输入远程URL地址！");
+                return;
+            }
+            switch (currentChoice)
+            {
+                case LINKTYPE.JPEG:
+                {
+                    
+                    // create video source
+                    JPEGStream jpegSource = new JPEGStream(url);
+
+                    // open it
+                    OpenVideoSource(jpegSource);
+                    break;
+                }
+                case LINKTYPE.MJPEG:
+                {
+                    // create video source
+                    MJPEGStream mjpegSource = new MJPEGStream(url);
+
+                    // open it
+                    OpenVideoSource(mjpegSource);
+                    break;
+                }
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCurrentVideoSource();
+        }
+
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            DateTime now = DateTime.Now;
+            Graphics g = Graphics.FromImage(image);
+
+            // paint current time
+            SolidBrush brush = new SolidBrush(Color.Red);
+            g.DrawString(now.ToString(), this.Font, brush, new PointF(5, 5));
+            brush.Dispose();
+
+            g.Dispose();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            IVideoSource videoSource = videoSourcePlayer.VideoSource;
+
+            if (videoSource != null)
+            {
+                // get number of frames since the last timer tick
+                int framesReceived = videoSource.FramesReceived;
+
+                if (stopWatch == null)
+                {
+                    stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                }
+                else
+                {
+                    stopWatch.Stop();
+
+                    float fps = 1000.0f * framesReceived / stopWatch.ElapsedMilliseconds;
+                    toolStripStatusLabel.Text = fps.ToString("F2") + " fps";
+
+                    stopWatch.Reset();
+                    stopWatch.Start();
+                }
+            }
+        }
+    }
+}
